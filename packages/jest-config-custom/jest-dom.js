12f42185ee2e8ff34eb2d37a1138babf4479d675
jocalu <jocalu87@gmail.com>
2023-03-01 14:42:59 +0100

    Remove useless config

--- packages/jest-config-custom/jest-dom.js
+++ packages/jest-config-custom/jest-dom.js
@@ -3,7 +3,6 @@ const sharedConfig = require('./jest-common')
 module.exports = {
   ...sharedConfig,
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   collectCoverageFrom: ['src/**/*.{js,ts}'],
   moduleFileExtensions: ['js', 'json', 'ts'],
   transform: {
