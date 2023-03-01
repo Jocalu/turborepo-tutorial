e928cedd924d966279249b1a9c7e4ef69fe1dac2
jocalu <jocalu87@gmail.com>
2023-03-01 10:26:54 +0100

    Add stylelint

---
+++ packages/stylelint-config-custom/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-concentric-order'],
+  rules: {
+    'selector-class-pattern': null,
+    'custom-property-pattern': null,
+    'color-function-notation': null
+  }
+}
