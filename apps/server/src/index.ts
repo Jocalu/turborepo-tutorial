ddf0ecc96d9795ec084f0623d3e126df264a21a6
jose.carcamo@gruposantander.com <jose.carcamo@gruposantander.com>
2023-04-18 15:24:20 +0200

    add info message for server entrypoint

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -13,6 +13,12 @@ app.use(cors())
 app.use('/api/pokemons', pokemonRouter)
 app.use('/api/characters', rickAndMortyRouter)
 
+app.use('/', (_, res) => {
+  res.send(
+    'This is the server for the pokemon and rick and morty app, please use the /api/pokemons or /api/characters endpoints'
+  )
+})
+
 app.listen(port, () => {
   console.log(`⚡️ Server is running at http://localhost:${port}`)
 })
