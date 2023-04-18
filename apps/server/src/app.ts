ab728d6fa40d4c59c7b7956a86c575b35d0b3d41
jose.carcamo@gruposantander.com <jose.carcamo@gruposantander.com>
2023-04-18 15:50:58 +0200

    export express app

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -6,7 +6,6 @@ import pokemonRouter from './router/pokemonRouter'
 import rickAndMortyRouter from './router/rickAndMortyRouter'
 
 const app: Express = express()
-const port = 4000
 
 app.use(cors())
 
@@ -19,6 +18,4 @@ app.use('/', (_, res) => {
   )
 })
 
-app.listen(port, () => {
-  console.log(`⚡️ Server is running at http://localhost:${port}`)
-})
+export default app
